612dd0676f1f80f574308b90868ec0fdbadd5762
Bill Kindle <wkindle@corsicatech.com>
2020-09-08 11:38:36 -0400

    reconfigured to be an advanced function.

--- Enable-PSTranscriptionLogging.ps1
+++ Enable-PSTranscriptionLogging.ps1
@@ -1,28 +1,47 @@
+function Enable-PSTranscriptionLogging {
+
 <#
     TO DO:
     - Turn this into an advanced function / add help
     - Add test for checking if properties are present
     - Add some validation tests?
-
+    - Requires Administrative rights - add a check for that. 
 #>
-function Enable-PSTranscriptionLogging {
-     # Registry path
-     $basePath = 'HKLM:\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\PowerShell\Transcriptions'
 
-     # Create the key if it does not exist
-     if(-not (Test-Path $basePath)) {
-         $null = New-Item $basePath -Force
+    [CmdletBinding()]
+    param (
+        [string[]]$Path
+    ) # End param
+    
+    begin {
+        
+        # Registry path
+        $basePath = 'HKLM:\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\PowerShell\Transcriptions'
 
-         # Enable the correct properties, be sure to change the output directory path before running function!
-         New-ItemProperty $basePath -Name "EnableInvocationHeader" -PropertyType Dword
-         New-ItemProperty $basePath -Name "EnableTranscripting" -PropertyType Dword
-         New-ItemProperty $basePath -Name "OutputDirectory" -PropertyType String
+        # Create the key if it does not exist
+        if(-not (Test-Path $basePath)) {
+            $null = New-Item $basePath -Force
 
-     }
+            # Enable the correct properties, be sure to change the output directory path before running function!
+            New-ItemProperty $basePath -Name "EnableInvocationHeader" -PropertyType Dword
+            New-ItemProperty $basePath -Name "EnableTranscripting" -PropertyType Dword
+            New-ItemProperty $basePath -Name "OutputDirectory" -PropertyType String
 
-     Set-ItemProperty $basePath -Name "EnableInvocationHeader" -Value "1"
-     Set-ItemProperty $basePath -Name "EnableTranscripting" -Value "1"
-     # This needs to be setup as a variable
-     Set-ItemProperty $basePath -Name "OutputDirectory" -Value "[YOUR PATH]"
+        }
 
-}
+    } # End begin
+    
+    process {
+
+        Set-ItemProperty $basePath -Name "EnableInvocationHeader" -Value "1"
+        Set-ItemProperty $basePath -Name "EnableTranscripting" -Value "1"
+        # This needs to be setup as a variable, maybe test / create path too?
+        Set-ItemProperty $basePath -Name "OutputDirectory" -Value "$Path"
+        
+    } # End process
+    
+    end {
+        
+    } # End end
+
+} # End fucntion
